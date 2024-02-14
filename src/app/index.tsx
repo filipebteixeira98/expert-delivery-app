819085167c3ede83e2fd321bce6304e7c4315c20
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-02-14 19:14:52 -0300

    chore: create category button component structure

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,15 @@
 import { View } from 'react-native'
 
 import { Header } from '@/components/header'
+import { CategoryButton } from '@/components/category-button'
 
 export default function Home() {
   return (
     <View className="flex-1 pt-8">
       <Header title="Make your wish" cartQuantityItems={1} />
+      <View className="flex-row gap-4">
+        <CategoryButton title="Snack of the day" isSelected />
+      </View>
     </View>
   )
 }
