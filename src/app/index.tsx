3d86d7c4e4f433ffd05562415234c230754011fc
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-02-14 22:24:34 -0300

    feat(app): add functionality that allows users to navigate through catalog product info page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import { useState, useRef } from 'react'
 import { FlatList, SectionList, Text, View } from 'react-native'
+import { Link } from 'expo-router'
 
 import { Header } from '@/components/header'
 import { CategoryButton } from '@/components/category-button'
@@ -51,7 +52,11 @@ export default function Home() {
         sections={MENU}
         keyExtractor={(item) => item.id}
         stickySectionHeadersEnabled={false}
-        renderItem={({ item }) => <Product data={item} />}
+        renderItem={({ item }) => (
+          <Link href={`/product/${item.id}`} asChild>
+            <Product data={item} />
+          </Link>
+        )}
         renderSectionHeader={({ section: { title } }) => (
           <Text className="text-white text-xl font-heading mt-8 mb-3">
             {title}
