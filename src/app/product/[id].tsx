c1a8f0e3dc700c053e067768d5d097cbd2f2fa9d
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-02-14 23:24:40 -0300

    refactor(product): to validate product id and redirects user to root screen if it is not

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,5 +1,5 @@
 import { Image, Text, View } from 'react-native'
-import { useLocalSearchParams, useNavigation } from 'expo-router'
+import { useLocalSearchParams, useNavigation, Redirect } from 'expo-router'
 import { Feather } from '@expo/vector-icons'
 
 import { useCartStore } from '@/stores/cart-store'
@@ -17,12 +17,18 @@ export default function Product() {
 
   const cartStore = useCartStore()
 
-  const product = PRODUCTS.filter((product) => product.id === id)[0]
+  const product = PRODUCTS.find((product) => product.id === id)
 
   function handleAddToCart() {
-    cartStore.add(product)
+    // if (product) {
+    cartStore.add(product!)
 
     navigation.goBack()
+    // }
+  }
+
+  if (!product) {
+    return <Redirect href="/" />
   }
 
   return (
@@ -33,6 +39,7 @@ export default function Product() {
         resizeMode="cover"
       />
       <View className="p-5 mt-8 flex-1">
+        <Text className="text-white text-xl font-heading">{product.title}</Text>
         <Text className="text-lime-400 text-2xl font-heading my-2">
           {formatCurrency(product.price)}
         </Text>
