a51276184f24e896fe40a007a6a046fd487cc9f0
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-02-15 01:20:52 -0300

    feat(app): add functionality that allows users to send order to company phone number through deep linking, clears products cart and redirects user to home page

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,5 +1,7 @@
-import { ScrollView, Text, View, Alert } from 'react-native'
+import { useState } from 'react'
+import { ScrollView, Text, View, Alert, Linking } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { useNavigation } from 'expo-router'
 import { Feather } from '@expo/vector-icons'
 
 import { ProductCartProps, useCartStore } from '@/stores/cart-store'
@@ -12,7 +14,13 @@ import { LinkButton } from '@/components/link-button'
 
 import { formatCurrency } from '@/utils/functions/format-currency'
 
+const PHONE_NUMBER = '5571991520664'
+
 export default function Cart() {
+  const [address, setAddress] = useState('')
+
+  const navigation = useNavigation()
+
   const cartStore = useCartStore()
 
   const totalPrice = formatCurrency(
@@ -32,6 +40,33 @@ export default function Cart() {
         onPress: () => cartStore.remove(product.id),
       },
     ])
+  }
+
+  function handleOrder() {
+    if (address.trim().length === 0) {
+      return Alert.alert('Order', 'Provide delivery details!')
+    }
+
+    const products = cartStore.products
+      .map((product) => `\n ${product.quantity} ${product.title}`)
+      .join('')
+
+    const message = `
+      🍔 NEW ORDER
+      \n Deliver to: ${address}
+
+      ${products}
+
+      \n Amount: ${totalPrice}
+    `
+
+    Linking.openURL(
+      `http://api.whatsapp.com/send?phone=${PHONE_NUMBER}&text=${message}`
+    )
+
+    cartStore.clear()
+
+    navigation.goBack()
   }
 
   return (
@@ -61,12 +96,18 @@ export default function Cart() {
                 {totalPrice}
               </Text>
             </View>
-            <Input placeholder="Enter the delivery address with street, neighborhood, zip code, number and complement" />
+            <Input
+              placeholder="Enter the delivery address with street, neighborhood, zip code, number and complement"
+              onChangeText={setAddress}
+              blurOnSubmit={true}
+              onSubmitEditing={handleOrder}
+              returnKeyType="next"
+            />
           </View>
         </ScrollView>
       </KeyboardAwareScrollView>
       <View className="p-5 gap-5">
-        <Button>
+        <Button onPress={handleOrder}>
           <Button.Text>Send order</Button.Text>
           <Button.Icon>
             <Feather name="arrow-right-circle" size={20} />
