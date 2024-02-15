4bfdf31ac30fca1f359ffea4abd8f82d86d16984
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-02-15 00:43:34 -0300

    feat(app): add functionality that allows users to click on cart product and select option from alert component to remove it

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,8 +1,8 @@
-import { ScrollView, Text, View } from 'react-native'
+import { ScrollView, Text, View, Alert } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import { Feather } from '@expo/vector-icons'
 
-import { useCartStore } from '@/stores/cart-store'
+import { ProductCartProps, useCartStore } from '@/stores/cart-store'
 
 import { Header } from '@/components/header'
 import { Product } from '@/components/product'
@@ -22,6 +22,18 @@ export default function Cart() {
     )
   )
 
+  function handleProductRemove(product: ProductCartProps) {
+    Alert.alert('Remove', `Do you want to remove ${product.title} from cart?`, [
+      {
+        text: 'Cancel',
+      },
+      {
+        text: 'Remove',
+        onPress: () => cartStore.remove(product.id),
+      },
+    ])
+  }
+
   return (
     <View className="flex-1 pt-8">
       <Header title="Your cart" />
@@ -31,7 +43,11 @@ export default function Cart() {
             {cartStore.products.length > 0 ? (
               <View className="border-b border-slate-700">
                 {cartStore.products.map((product) => (
-                  <Product key={product.id} data={product} />
+                  <Product
+                    key={product.id}
+                    data={product}
+                    onPress={() => handleProductRemove(product)}
+                  />
                 ))}
               </View>
             ) : (
