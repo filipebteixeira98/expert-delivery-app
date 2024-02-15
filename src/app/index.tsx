ddb986bb283a3c2fc030c0c3cf179240305d872c
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-02-14 23:19:08 -0300

    fix(app): to add generics type to section list which handles with autocomplete from intellisense

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -8,12 +8,12 @@ import { Header } from '@/components/header'
 import { CategoryButton } from '@/components/category-button'
 import { Product } from '@/components/product'
 
-import { CATEGORIES, MENU } from '@/utils/data/products'
+import { CATEGORIES, MENU, ProductProps } from '@/utils/data/products'
 
 export default function Home() {
   const [category, setCategory] = useState(CATEGORIES[0])
 
-  const sectionListRef = useRef<SectionList>(null)
+  const sectionListRef = useRef<SectionList<ProductProps>>(null)
 
   const cartStore = useCartStore()
 
